af359e5ca68a27212c2e9f07514dbcd5373d5ee9
KARAN RUDOLA <144210797+KaranRudola@users.noreply.github.com>
2026-05-21 16:55:59 +0530

    Redirect root URL to login page

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from django.shortcuts import redirect
 
 urlpatterns = [
+    path('', lambda request: redirect('login')),  # root goes to login
     path('register/', views.register, name='register'),
     path('login/', views.login_view, name='login'),
     path('dashboard/', views.dashboard, name='dashboard'),
